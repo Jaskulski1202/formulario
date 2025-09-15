e6a4ebc02e5b5c1084507bcac2b5d4af78831f39
Jaskulski1202 <jaskulski.silva@escola.pr.gov.br>
2025-09-15 11:28:24 -0300

    botão

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ import { PALAVRAS_RUINS } from "./palavrasRuins2.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+function mostraPalavrasChave() {
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
 
+    campoResultado.textContent = palavrasChave.join(", ");
+}
 function mostraPalavrasChave() { 
 let palavras = texto.split(/\P{L}+/u);
 
@@ -14,3 +20,10 @@ let palavras = texto.split(/\P{L}+/u);
 
     const frequencias = contaFrequencias(palavras);
 } 
+function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+}
